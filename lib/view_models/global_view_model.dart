2255d3b6239431a0fe6256669ccf8b13f966a569
Mahmoud El Shenawy <59105177+Mahmoud-ElShenawy@users.noreply.github.com>
2021-01-06 17:03:07 +0200

    Update global_view_model.dart

--- lib/view_models/global_view_model.dart
+++ lib/view_models/global_view_model.dart
@@ -2,6 +2,8 @@ import 'package:coronavirus_app/models/global_summary_model.dart';
 import 'package:coronavirus_app/services/coronavirus_api.dart';
 import 'package:flutter/foundation.dart';
 
+// Created By Mahmoud El Shenawy (Email : Mr.Mahmoud.El.Shenawy@Gmail.com)
+
 class GlobalViewModel extends ChangeNotifier {
   GlobalSummaryModel _globalSummaryList;
 
