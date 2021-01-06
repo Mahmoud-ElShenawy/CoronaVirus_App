79ea813a02ad670078bcd8456d51a81bc7a99056
Mahmoud El Shenawy <59105177+Mahmoud-ElShenawy@users.noreply.github.com>
2021-01-06 17:01:55 +0200

    Update chart.dart

--- lib/views/widgets/chart.dart
+++ lib/views/widgets/chart.dart
@@ -2,6 +2,8 @@ import 'package:charts_flutter/flutter.dart' as Charts;
 import 'package:coronavirus_app/models/time_cases_model.dart';
 import 'package:flutter/material.dart';
 
+// Created By Mahmoud El Shenawy (Email : Mr.Mahmoud.El.Shenawy@Gmail.com)
+
 class Chart extends StatelessWidget {
   final List<Charts.Series<TimeCasesModel, DateTime>> seriesList;
   final bool animate;
