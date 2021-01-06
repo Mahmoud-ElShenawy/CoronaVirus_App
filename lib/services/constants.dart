5fae32dd9c8c9cafd3e4b48295ac84765556119f
Mahmoud El Shenawy <59105177+Mahmoud-ElShenawy@users.noreply.github.com>
2021-01-06 17:04:03 +0200

    Update constants.dart

--- lib/services/constants.dart
+++ lib/services/constants.dart
@@ -1,3 +1,5 @@
+// Created By Mahmoud El Shenawy (Email : Mr.Mahmoud.El.Shenawy@Gmail.com)
+
 class Constants{
   static const globalSummaryCases = 'https://api.covid19api.com/summary';
   static const countrySummaryCases = 'https://api.covid19api.com/total/dayone/country/';
